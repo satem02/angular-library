22f95cb603a1da8fd9f030fd5922db7397700336
Şafak <safak.temel@outlook.com>
2019-11-05 11:52:39 +0300

    Update child-base-component.ts

--- projects/satem-ui/src/lib/components/parent-child-examples/parent-to-child/base/child-base-component.ts
+++ projects/satem-ui/src/lib/components/parent-child-examples/parent-to-child/base/child-base-component.ts
@@ -1,24 +1,35 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { FormGroup } from "@angular/forms";
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectionStrategy
+} from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: "satem-ui-child-base"
+  selector: "satem-ui-child-base",
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ChildBaseComponent implements OnInit {
-    
   @Input() parentFormGroup: FormGroup;
   @Input() childPropertyName: string;
   private childFormGroup: FormGroup;
-
   constructor() {}
 
   ngOnInit() {}
 
-  public addGroupToParent(form: FormGroup) {
+  public addGroupToParentForm(form: FormGroup) {
     this.childFormGroup = form;
     this.parentFormGroup.addControl(
       this.childPropertyName,
       this.childFormGroup
+    );
+  }
+
+  public addGroupToControl(data: Object) {
+    this.parentFormGroup.addControl(
+      this.childPropertyName,
+      new FormControl(data)
     );
   }
 }
